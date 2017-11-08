b530ae062833c1cc4391708580cb583d49192a93
Kostas Stathis <kostas.stathis@googlemail.com>
2017-11-08 13:23:09 +0000

    Added Java doc.

--- src/main/java/uk/ac/rhul/cs/dice/agentactions/enums/ActionResult.java
+++ src/main/java/uk/ac/rhul/cs/dice/agentactions/enums/ActionResult.java
@@ -1,5 +1,16 @@
 package uk.ac.rhul.cs.dice.agentactions.enums;
 
+import uk.ac.rhul.cs.dice.agentcommon.interfaces.Action;
+
+/**
+ * 
+ * <code>IMPOSSIBLE</code> means that the {@link Action} preconditions are not met. <br/>
+ * <code>SUCCESS</code> means that both the {@link Action} preconditions and postconditions are met. <br/>
+ * <code>FAILURE</code> means that the {@link Action} preconditions are met but the postconditions are not met.
+ * 
+ * @author kostas
+ *
+ */
 public enum ActionResult {
     IMPOSSIBLE, SUCCESS, FAILURE;
 }
