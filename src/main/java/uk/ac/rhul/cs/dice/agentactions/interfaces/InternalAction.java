b179861eda9ac84abb5eb6326a54ae5d303fc222
Kostas Stathis <kostas.stathis@googlemail.com>
2017-11-14 11:19:35 +0000

    "$COMMENT"

--- src/main/java/uk/ac/rhul/cs/dice/agentactions/interfaces/InternalAction.java
+++ src/main/java/uk/ac/rhul/cs/dice/agentactions/interfaces/InternalAction.java
@@ -8,6 +8,7 @@ import uk.ac.rhul.cs.dice.agentcommon.interfaces.Action;
  * Interface for internal {@link Action}s. It extends {@link Action}.
  * 
  * @author cloudstrife9999
+ * @author kostas
  *
  * @param <T> an enumeration.
  * 
@@ -16,7 +17,7 @@ public interface InternalAction<T extends Enum<?>> extends Action<T> {
     
     /**
      * 
-     * Attempts this action in a {@link Mind}.
+     * Attempts this action in a {@link Mind}. It indicates that this {@link InternalAction} might raise an {@link Exception}.
      * 
      * @param mind a {@link Mind}.
      * 
